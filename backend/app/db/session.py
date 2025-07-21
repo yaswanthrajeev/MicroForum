ef1c9ab4e3e6fcceedcadf1bba5804332794594e
yaswanthrajeev <yaswanthrajeev@gmail.com>
2025-07-22 02:52:00 +0530

    migrated to postgress

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///forum.db"
+SQLALCHEMY_DATABASE_URL="postgresql+psycopg2://yaswanth:#kerala1*@localhost:5432/microforum"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
